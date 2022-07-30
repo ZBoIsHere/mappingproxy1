6d019aae257fbf7b7e7bc944c15d234f701d3670
ZBoIsHere <13126896150@163.com>
2022-07-30 15:30:58 +0800

    refactor some logic

--- src/mapcontroller.cpp
+++ src/mapcontroller.cpp
@@ -6,7 +6,6 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include "boost/filesystem.hpp"
 
-
 const char* ENV_CLOUDMAPPATH = "CLOUD_MAP_PATH";
 const char* ENV_CLOUDMAPNAME = "CLOUD_MAP_NAME";
 char* cloud_map_path;
@@ -26,11 +25,13 @@ int main(int argc, char** argv) {
     cloud_map_path = getenv(ENV_CLOUDMAPPATH);
     if (cloud_map_path == nullptr) {
         ROS_ERROR("Can not get env %s", ENV_CLOUDMAPPATH);
+        return -1;
     }
 
     cloud_map_name = getenv(ENV_CLOUDMAPNAME);
     if (cloud_map_name == nullptr) {
         ROS_ERROR("Can not get env %s", ENV_CLOUDMAPNAME);
+        return -1;
     }
 
     colored_pointcloud_sub = nh.subscribe("colored_pointcloud_pub", 100, colored_pointcloud_callback);
@@ -65,8 +66,24 @@ void colored_pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr& input
 
 void uncolored_pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr& input) {
     pcl::PointCloud<pcl::PointXYZI> output_ptr ;
+
+    if (!boost::filesystem::is_directory(cloud_map_path)) {
+        ROS_INFO("start create path %s", cloud_map_path);
+        if (!boost::filesystem::create_directories(cloud_map_path)) {
+            ROS_INFO("failed create path %s", cloud_map_path);
+        }
+    }
+
+    std::string pcd_file = std::string(cloud_map_path).append(cloud_map_name);
+
     pcl::fromROSMsg(*input, output_ptr);
-    if(pcl::io::savePCDFileBinary("/root/rosbag/test.pcd", output_ptr) == 0) {
+    if (output_ptr.empty()) {
+        ROS_WARN("point cloud has no data");
+        return;
+    }
+
+    if(pcl::io::savePCDFileBinary(pcd_file, output_ptr) == 0) {
         ROS_INFO("save pcd file success");
+        ros::shutdown();
     }
 }
--- src/rawdatacontroller.cpp
+++ src/rawdatacontroller.cpp
@@ -2,10 +2,12 @@
 #include "ros/ros.h"
 #include "rosbag/player.h"
 #include "std_msgs/String.h"
-#include "../include//mappingproxy/rawdatacontroller.h"
 #include "rosbag/bag.h"
 #include "rosbag/view.h"
 #include "thread"
+
+void algorithmStartedCallback(const std_msgs::String::ConstPtr& msg);
+void rawdataPlayCountdown();
 
 ros::Publisher rawdata_started_pub;
 ros::Publisher rawdata_finished_pub;
@@ -13,8 +15,6 @@ ros::Subscriber algorithm_started_sub;
 
 const char* ENV_RWADATAFILE = "RAWDATAFILE";
 char* rawdata_file;
-
-void rawdataPlayCountdown();
 
 int main(int argc, char** argv) {
     ROS_INFO("rawdata controller start");
@@ -24,23 +24,25 @@ int main(int argc, char** argv) {
     rawdata_file = getenv(ENV_RWADATAFILE);
     if (rawdata_file == nullptr) {
         ROS_ERROR("Can not get env %s", ENV_RWADATAFILE);
+        return -1;
     }
 
     // todo zhangbo how to set reasonable queue_size
     rawdata_started_pub = nh.advertise<std_msgs::String>("rawdata_started_pub", 100);
     rawdata_finished_pub = nh.advertise<std_msgs::String>("rawdata_finished_pub", 100);
-    algorithm_started_sub = nh.subscribe("algorithm_started_pub", 100, algorithmStatusCallback);
+    algorithm_started_sub = nh.subscribe("algorithm_started_pub", 100, algorithmStartedCallback);
     ros::spin();
     return 0;
 }
 
-void algorithmStatusCallback(const std_msgs::String::ConstPtr& msg) {
+void algorithmStartedCallback(const std_msgs::String::ConstPtr& msg) {
     rosbag::Bag bag;
     bag.open(rawdata_file, rosbag::bagmode::Read);
     rosbag::View view(bag);
     auto bag_length_time = view.getEndTime() - view.getBeginTime();
     auto bag_length_time2sec = bag_length_time.toSec();
     std::thread thr1(rawdataPlayCountdown);
+    // todo zhangbo
     ros::Duration(bag_length_time2sec - 3).sleep();
     std_msgs::String rawdata_status;
     rawdata_status.data = "FINISHED";
